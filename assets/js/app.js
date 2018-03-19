692a6d79542e9fe2bc1e17e416b3df2673a740b1
AriellaRogueN <arrodriguezn@gmail.com>
2018-03-18 23:48:12 -0300

    Arreglando modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         var latitud = locale.geometry.location.lat();
         var longitud = locale.geometry.location.lng();
         /*Realizamos llamado a api dark sky, se incluye Jsonp, para quitar seguridad,
-        agregamos variables latitud y longitud*/
+        agregamos variables para obtener datos*/
         $.ajax({
             url: "https://api.darksky.net/forecast/1578303b8a10e722c3a97c9a2c96796a/" + latitud + "," + longitud,
             dataType: "jsonp",
@@ -33,7 +33,8 @@ $(document).ready(function () {
                 console.log(pressure)
                 const weekSummary = data.daily.summary
                 console.log(weekSummary)
-
+             /*Al hacer click en el boton de busqueda se agrega una estructura HTML
+             con las variables correspondientes, para mostrar los datos*/
                 $("#btn-search").click(function () {
                     $(".bewitched").append("<div class='row'>" +
                         "<div class='col-md-offset-3 col-md-6 col-sm-12 col-xs-12 day'>" +
@@ -46,36 +47,46 @@ $(document).ready(function () {
                         "<li>Uv index:" + "&nbsp" + uv + "</li>" +
                         "<li>Wind:" + "&nbsp" + wind + "&nbsp" + "m/s</li>" +
                         "<li>Pressure:" + "&nbsp" + pressure + "&nbsp" + "Hpa</li>" + "<hr>" +
-                        "<button type='button' id='btn-weekly' class='btn btn-info btn-md ' data-toggle='modal' data-target='#myModal'>Weekly Report</button>" +
+                        "<button type='button' id='btn-weekly' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Weekly Report</button>" +
                         "</ul></div></div>")
-                });//append-1
-                $("#btn-weekly").click(function() {
-                    $("#myModal").modal("show");
-                    for (i = 1; i < data.daily.data.length; i++) {
-                        const minTemp = data.daily.data[i].temperatureMin;
-                        const maxTemp = data.daily.data[i].temperatureHigh;
-                        const time = data.daily.data[i].time;
-                        /*La data time corresponde a fecha-hora en formato unix,
-                        para poder mostrarla de manera legible,debe ser transformada 
-                        en formato normal,  para lo cual realizamos una fórmula*/
-                        const convert = new Date(time * 1000);
-                        const newTime = new Date(convert);
-                        console.log(newTime)
-                        $(".modal-dialog").append("<div class='modal-content'>" +
-                                                  "<div class='modal-header'>" +
-                                                  "<button type='button' class='close' data-dismiss='modal'>&times;</button>" +
-                                                  "<h4 class='modal-title'>Modal Header</h4>" + "</div>" +
-                                                  "<div class='modal-body'>" +
-                                                  "<p>Some text the modal</p>" + "</div>" +
-                                                  "<div class='modal-footer'>" +
-                                                  "<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>" +
-                                                  "</div></div></div></div>");
- 
 
-                    };//for
-                    
-                });//click
+                    $("#btn-weekly").click(function () {
+                        console.log("boton pulsado");
+                        for (i = 1; i < data.daily.data.length; i++) {
+                            const minTemp = data.daily.data[i].temperatureMin;
+                            const maxTemp = data.daily.data[i].temperatureHigh;
+                            const time = data.daily.data[i].time;
+                            /*La data time corresponde a fecha-hora en formato unix,
+                            para poder mostrarla de manera legible,debe ser transformada 
+                            en formato normal,  para lo cual realizamos una fórmula*/
+                            const convert = new Date(time * 1000);
+                            /*Luego aplicamos un metodo para acortar los valores, 
+                            quedando nombre de dia de la semana,dia numerico y mes */
+                            const details =  {
+                                month: "short",
+                                weekday: 'short',
+                                day: 'numeric' 
+                            };
+                            const newTime = new Date(convert);
+                            console.log(newTime)
+                            $("#mdl").append("<div id='myModal' class='modal fade' role='dialog' tabindex='-1'>" +
+                                "<div class='modal-dialog'>" +
+                                "<div class='modal-content'>" +
+                                "<div class='modal-header'>" +
+                                "<button type='button' class='close' data-dismiss='modal'>&times;</button>" +
+                                "<h4 class='modal-title'>" + city + "</h4>" + "</div>" +
+                                "<div class='modal-body'>" +
+                                "<h5>" + weekSummary  + "</h5>" +
+                                "<p>" + newTime.toLocaleDateString('en-US', details) + "&nbsp" + "<span>-" + "&nbsp" + ((maxTemp - 32) / 1.8).toFixed() + "°C /" + ((minTemp - 32) / 1.8).toFixed() + "°C</span>" + "</p>" + 
+                                 "</div>" +
+                                "</div></div></div>");
+                        };    
+                          
+
+                    });//click
+                });//append-1
                 
+                 
             }//succes
         });//ajax
 
